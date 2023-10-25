230a961e153da348c90d7f2b3e677aaa345bab8d
JESSICA MAYARA GERMANO PESSOA <jessica.pessoa@academico.ifrn.edu.br>
2023-10-25 13:27:42 -0300

    tiroboss2

--- JogoOficinaV/Assets/Scripts/Boss2.cs
+++ JogoOficinaV/Assets/Scripts/Boss2.cs
@@ -1,34 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
 
 public class Boss2 : MonoBehaviour
 {
     public float speed;
     public float walkTime;
+    public bool walkRigth = true;
+    private float tempTiro;
+    public float tempTiroMax = 2;
+    public GameObject prefabDeTiro;
 
-    private bool Timer;
-    private bool walkRigth;
-    private Rigidbody rig;
+    private float timer;
 
-    // Start is called before the first frame update
+    private Rigidbody2D rig;
+
+    private bool ataqueEmAndamento = false;
+
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+
     }
 
-    // Update is called once per frame
+    IEnumerator SequenciaAtaque()
+    {
+       
+        GetComponent<Animator>().Play("attack");
+        yield return new WaitForSeconds(0.3f);
+        
+        //aqui que vai instanciar o tiro
+        GameObject clone = Instantiate(prefabDeTiro, transform.GetChild(0).position, Quaternion.identity);
+        if (!walkRigth)
+        {
+            clone.transform.Rotate(Vector3.up,180 );
+        }
+        yield return new WaitForSeconds(0.1f);
+        GetComponent<Animator>().Play("walk");
+        tempTiro = 0;
+        ataqueEmAndamento = false;
+        StopCoroutine(SequenciaAtaque());
+    }
+
+
     void FixedUpdate()
     {
-        Timer += Time.deltaTime;
-        if(Timer >= walkTime)
+        timer += Time.deltaTime;
+        tempTiro += Time.deltaTime;
+    
+        if (timer >= walkTime)
         {
-            Timer = 0f;
+            walkRigth = !walkRigth;
+            timer = 0f;
         }
 
-        if (walkRigth)
+        if (tempTiro >= tempTiroMax && !ataqueEmAndamento)
         {
-            
+            ataqueEmAndamento = true;
+            StartCoroutine(SequenciaAtaque());
+        }
+
+        if (ataqueEmAndamento)
+        {
+            rig.velocity = Vector2.zero;
+        }
+        else
+        {
+            if (walkRigth)
+                    {
+                        transform.eulerAngles = new Vector2(0, 0);
+                        rig.velocity = Vector2.right * speed;
+                    }
+                    else
+                    {
+                        transform.eulerAngles = new Vector2(0, 180);
+                        rig.velocity = Vector2.left * speed;
+                    }
         }
     }
+
 }
